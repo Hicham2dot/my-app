fc613921ab1b5fc5237fd267bfb66b586ab46b0c
ton-username <ton-email@example.com>
2025-12-27 22:17:19 +0100

    run both

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+    {
+        name: "Server",
+        command:"bun run dev",
+        cwd: "packages/server",
+        prefixColor: "blue",
+    },
+    {
+        name: "Client",
+        command:"bun run dev",
+        cwd: "packages/client",
+        prefixColor: "green",
+    }
+
+])
